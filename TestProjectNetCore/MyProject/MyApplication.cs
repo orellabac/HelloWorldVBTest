40516e2ce553216b10e8a324ee3591b0d22263da
Mauricio Rojas <Mauricio.Rojas@mobilize.net>
2020-05-25 16:16:55 -0600

    tets with extracted helpers

---
+++ TestProjectNetCore/MyProject/MyApplication.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+using Mobilize.Helpers;
+
+namespace HelloWorld
+{
+    namespace My
+    {
+
+        //remove //NOTE: This file is auto-generated; do not modify it directly.  To make changes,
+        //remove // or if you encounter build errors in this file, go to the Project Designer
+        //remove // (go to Project Properties or double-click the My Project node in
+        //remove // Solution Explorer), and make changes on the Application tab.
+        //remove //
+        internal partial class MyApplication : WindowsFormsApplicationBase
+        {
+
+            //remove [global::System.Diagnostics.DebuggerStepThroughAttribute()]
+            public MyApplication()
+            : base(AuthenticationMode.Windows)
+            {
+                this.IsSingleInstance = false;
+                this.EnableVisualStyles = true;
+                this.SaveMySettingsOnExit = true;
+                this.ShutdownStyle = ShutdownMode.AfterMainFormCloses;
+            }
+
+            //remove [global::System.Diagnostics.DebuggerStepThroughAttribute()]
+            protected override void OnCreateMainForm()
+            {
+                this.MainForm = MyHelpers.MyProject.MyForms.Form<Form1>();
+                this.MainForm.Show();
+            }
+
+
+            public static void Main(string[] args)
+            {
+                new MyApplication().Run(args);
+            }
+        }
+
+
+
+    }
+}
